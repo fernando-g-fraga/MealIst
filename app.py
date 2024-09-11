6bcadeab16437dde20ad0d7a9aba98002d167139
fernando fraga <fernandogrfraga@gmail.com>
2024-09-11 16:54:30 -0300

    Minor changes on folder struct

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ from handler import todoist
 from app_middlewere import splitResponse
 
 def coletaReceitas()->list[str]:
-    recipe_list = ["Macarrão com molho de tomate","Bolo de cenoura","Pão de queijo"]
+    recipe_list = []
     
-    # print("Bem vindo! Este aplicativo foi feito para gerar uma lista de compras com base nas receitas solicitadas.")
+    print("Bem vindo! Este aplicativo foi feito para gerar uma lista de compras com base nas receitas solicitadas.")
     
 
-    # for i in range(1,4):
-    #     user_input = input(f"Por favor, insira abaixo o nome da receita que gostaria de agendar. Case deseje encerrar digite 0 \n")
-    #     if  user_input ==  "0":
-    #         break
-    #     recipe_list.append(user_input)
-    #     print(f"Receita {user_input} adicionada com sucesso. {i}/5")
+    for i in range(1,4):
+        user_input = input(f"Por favor, insira abaixo o nome da receita que gostaria de agendar. Case deseje encerrar digite 0 \n")
+        if  user_input ==  "0":
+            break
+        recipe_list.append(user_input)
+        print(f"Receita {user_input} adicionada com sucesso. {i}/5")
     return recipe_list
 
 lista_receitas = coletaReceitas()
